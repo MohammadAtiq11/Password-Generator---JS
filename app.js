f92c36e7950f221b2537f06d674943413c3266e1
Mohammad Atiq <154668693+MohammadAtiq11@users.noreply.github.com>
2024-05-23 00:53:08 +0500

    Password Generator - JS

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function generatePassword() {
+    const length = document.getElementById('passwordLength').value;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    document.getElementById('gen-pass').innerText = password;
+}
